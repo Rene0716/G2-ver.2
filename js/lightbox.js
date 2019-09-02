dcce2a4cd0ba058a3eac3a780f0d08e669704e1f
Rene0716 <a25324670@gmail.com>
2019-09-02 20:17:25 +0800

    1. 改:canvas背景色更換 2.增:愛心收藏紐點了跳會員登入的燈箱

---
+++ js/lightbox.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.collect').click(function(){
+        $('.lightBox').css('display','block');
+    });
+    $('btnLoginCancel img').click(function(){
+        $('.lightBox').css*('display','none');
+    })
+});
