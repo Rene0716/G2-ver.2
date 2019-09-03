dbeba4e528b744fea7772bd95023fa1ab32a2f3f
Rene0716 <a25324670@gmail.com>
2019-09-03 10:57:54 +0800

    更新

--- js/lightbox.js
+++ js/lightbox.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.collect').click(function(){
         $('.lightBox').css('display','block');
     });
-    $('btnLoginCancel img').click(function(){
-        $('.lightBox').css*('display','none');
+    $('.btnLoginCancel img').click(function(){
+        $('.lightBox').css('display','none');
     })
 });
